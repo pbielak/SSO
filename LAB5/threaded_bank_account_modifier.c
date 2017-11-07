e243c4ed6e42fbcb6e15f19c6bd67943ba3ee570
Piotr Bielak <piotrbielak2@gmail.com>
2017-11-07 13:27:31 +0100

    Changed hardcoded args to getopt

--- LAB5/threaded_bank_account_modifier.c
+++ LAB5/threaded_bank_account_modifier.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <assert.h>
+#include <ctype.h>
 
 #define NB_ACCOUNTS 2
 
@@ -21,32 +22,17 @@ pthread_cond_t operation_cond, not_enough[NB_ACCOUNTS];
 void* run_withdraw(void* args);
 
 int main(int argc, char* argv[]) {
-  int nb_threads_0, nb_threads_1, nb_threads_transfer;
-  int nb_steps_0, nb_steps_1, nb_steps_transfer;
-  int cash_amount_0, cash_amount_1, cash_amount_transfer;
-
   int i;
+  char c;
 
-  pthread_t* account_0_threads;
-  pthread_t* account_1_threads;
+  pthread_t* threads;
+  int nb_threads;
   struct withdraw_args* w_args;
 
-  if (argc < 10) {
-   fprintf(stderr, "%s nb_threads_0 cash_amount_acc_0 steps_0 nb_threads_1 cash_amount_acc_1 steps_1 nb_threads_transfer cash_amount_transfer steps_transfer\n", argv[0]);
-     exit(-1);
+  if (argc < 5) {
+    fprintf(stderr, "%s -N nb_threads [-W account_number~cash_amount~nb_steps]\n", argv[0]);
+    exit(-1);
   }
-
-  nb_threads_0 = atoi(argv[1]);
-  cash_amount_0 = atoi(argv[2]);
-  nb_steps_0 = atoi(argv[3]);
-
-  nb_threads_1 = atoi(argv[4]);
-  cash_amount_1 = atoi(argv[5]);
-  nb_steps_1 = atoi(argv[6]);
-
-  nb_threads_transfer = atoi(argv[7]);
-  cash_amount_transfer = atoi(argv[8]);
-  nb_steps_transfer = atoi(argv[9]);
 
   // Init mutex and cond_var
   pthread_mutex_init(&mtx_lock, NULL);
@@ -54,42 +40,36 @@ int main(int argc, char* argv[]) {
   pthread_cond_init(&not_enough[0], NULL);
   pthread_cond_init(&not_enough[1], NULL);
 
-  // Run account 0 threads
-  account_0_threads = (pthread_t*) malloc(sizeof(pthread_t*) * nb_threads_0);
-  for(i = 0; i < nb_threads_0; i++) {
-    w_args = (struct withdraw_args*) malloc(sizeof(struct withdraw_args));
-    w_args->thread_id = i;
-    w_args->account_number = 0;
-    w_args->cash_amount = cash_amount_0;
-    w_args->steps = nb_steps_0;
+  opterr = 0;
+  i = 0;
 
-    printf("[%d/%d] Starting withdraw on account 0\n", i + 1, nb_threads_0);
+  while((c = getopt(argc, argv, "N:W:")) != -1) {
+   switch(c) {
+    case 'N':
+      nb_threads = atoi(optarg);
+      threads = (pthread_t*) malloc(sizeof(pthread_t*) * nb_threads);
+      printf("Got %d threads\n", nb_threads);
+      break;
 
-    pthread_create(&account_0_threads[i], NULL, run_withdraw, (void*) w_args);
+    case 'W':
+      if (threads == NULL) {
+        fprintf(stderr, "First parameter must be -N nb_threads");
+        exit(-1);
+      }
+
+      w_args = (struct withdraw_args*) malloc(sizeof(struct withdraw_args));
+      w_args->thread_id = i;
+      sscanf(optarg, "%d~%d~%d", &(w_args->account_number), &(w_args->cash_amount), &(w_args->steps));
+      printf("[%d/%d] Starting withdraw on account %d: Cash: %d Steps: %d\n", i + 1, nb_threads, w_args->account_number, w_args->cash_amount, w_args->steps);
+      pthread_create(&threads[i], NULL, run_withdraw, (void*) w_args);
+      i++;
+
+      break;
+   } 
   }
 
-  // Run account 1 threads 
-  account_1_threads = (pthread_t*) malloc(sizeof(pthread_t*) * nb_threads_1);
-  for(i = 0; i < nb_threads_1; i++) {
-    w_args = (struct withdraw_args*) malloc(sizeof(struct withdraw_args));
-    w_args->thread_id = i;
-    w_args->account_number = 1;
-    w_args->cash_amount = cash_amount_1;
-    w_args->steps = nb_steps_1;
-
-    printf("[%d/%d] Starting withdraw on account 1\n", i + 1, nb_threads_1);
-
-    pthread_create(&account_1_threads[i], NULL, run_withdraw, (void*) w_args);
-  }
-
-
-  // Join account 0 threads
-  for(i = 0; i < nb_threads_0; i++) {
-    pthread_join(account_0_threads[i], NULL);
-  }
-  // Join account 1 threads
-  for(i = 0; i < nb_threads_1; i++) {
-    pthread_join(account_1_threads[i], NULL);
+  for(i = 0; i < nb_threads; i++) {
+    pthread_join(threads[i], NULL);
   }
 
   // Print end balance
@@ -100,9 +80,6 @@ int main(int argc, char* argv[]) {
   pthread_cond_destroy(&operation_cond);
   pthread_cond_destroy(&not_enough[0]);
   pthread_cond_destroy(&not_enough[1]);
-
-  free(account_0_threads);
-  free(account_1_threads);
 
   return 0;
 }
@@ -115,7 +92,7 @@ void* run_withdraw(void* args) {
   for(i = 0; i < w_args->steps; i++) {
     // Semafor wait
     pthread_mutex_lock(&mtx_lock);
-      while(bank_accounts_balance[w_args->account_number] + w_args->cash_amount <= 0) {
+      while(bank_accounts_balance[w_args->account_number] + w_args->cash_amount < 0) {
         pthread_cond_wait(&not_enough[w_args->account_number], &mtx_lock);
       }
       while(sem[w_args->account_number] <= 0) {
