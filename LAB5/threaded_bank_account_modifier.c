827f614b048da1ff8bf4e227ad946179e391e660
Piotr Bielak <piotrbielak2@gmail.com>
2017-11-13 18:40:12 +0100

    Added function failure handling

--- LAB5/threaded_bank_account_modifier.c
+++ LAB5/threaded_bank_account_modifier.c
@@ -29,11 +29,12 @@ int bank_accounts_balance[NB_ACCOUNTS] = {0, 0};
 pthread_mutex_t mtx_lock;
 pthread_cond_t operation_cond, not_enough[NB_ACCOUNTS];
 
+void may_die(int res, char* cause);
 void* run_withdraw(void* args);
 void* run_transfer(void* args);
 
 int main(int argc, char* argv[]) {
-  int i;
+  int i, res;
   char c;
 
   pthread_t* threads;
@@ -47,10 +48,14 @@ int main(int argc, char* argv[]) {
   }
 
   // Init mutex and cond_var
-  pthread_mutex_init(&mtx_lock, NULL);
-  pthread_cond_init(&operation_cond, NULL);
-  pthread_cond_init(&not_enough[0], NULL);
-  pthread_cond_init(&not_enough[1], NULL);
+  res = pthread_mutex_init(&mtx_lock, NULL);
+  may_die(res, "pthread_mutex_init");
+  res = pthread_cond_init(&operation_cond, NULL);
+  may_die(res, "pthread_cond_init operation_cond");
+  res = pthread_cond_init(&not_enough[0], NULL);
+  may_die(res, "pthread_cond_init not_enough[0]");
+  res = pthread_cond_init(&not_enough[1], NULL);
+  may_die(res, "pthread_cond_init not_enough[1]");
 
   opterr = 0;
   i = 0;
@@ -73,7 +78,8 @@ int main(int argc, char* argv[]) {
       w_args->thread_id = i;
       sscanf(optarg, "%d~%d~%d", &(w_args->account_number), &(w_args->cash_amount), &(w_args->steps));
       printf("[%d/%d] Starting withdraw on account %d: Cash: %d Steps: %d\n", i + 1, nb_threads, w_args->account_number, w_args->cash_amount, w_args->steps);
-      pthread_create(&threads[i], NULL, run_withdraw, (void*) w_args);
+      res = pthread_create(&threads[i], NULL, run_withdraw, (void*) w_args);
+      may_die(res, "pthread_create withdraw");
       i++;
 
       break;
@@ -88,24 +94,26 @@ int main(int argc, char* argv[]) {
       t_args->thread_id = i;
       sscanf(optarg, "%d~%d~%d~%d", &(t_args->account_from), &(t_args->account_to), &(t_args->cash_amount), &(t_args->steps));
       printf("[%d/%d] Starting transfer on accounts: %d --> %d: Cash: %d Steps: %d\n", i + 1, nb_threads, t_args->account_from, t_args->account_to, t_args->cash_amount, t_args->steps);
-      pthread_create(&threads[i], NULL, run_transfer, (void*) t_args);
+      res = pthread_create(&threads[i], NULL, run_transfer, (void*) t_args);
+      may_die(res, "pthread_create transfer");
       i++;
       break;
    } 
   }
 
   for(i = 0; i < nb_threads; i++) {
-    pthread_join(threads[i], NULL);
+    res = pthread_join(threads[i], NULL);
+    may_die(res, "pthread_join");
   }
 
   // Print end balance
   printf("Final account balance: [%d][%d]\n", bank_accounts_balance[0], bank_accounts_balance[1]);
 
   // Clean up
-  pthread_mutex_destroy(&mtx_lock);
-  pthread_cond_destroy(&operation_cond);
-  pthread_cond_destroy(&not_enough[0]);
-  pthread_cond_destroy(&not_enough[1]);
+  res = pthread_mutex_destroy(&mtx_lock); may_die(res, "pthread_mutex_destroy");
+  res = pthread_cond_destroy(&operation_cond); may_die(res, "pthread_cond_destroy operation_cond");
+  res = pthread_cond_destroy(&not_enough[0]); may_die(res, "pthread_cond_destroy not_enough[0]");
+  res = pthread_cond_destroy(&not_enough[1]); may_die(res, "pthread_cond_destroy not_enough[1]");
 
   free(threads);
   return 0;
@@ -115,24 +123,28 @@ int main(int argc, char* argv[]) {
 void* run_withdraw(void* args) {
   struct withdraw_args* w_args = (struct withdraw_args*) args;
   int i, prev_balance;
-  int h;
+  int h, res;
 
   for(i = 0; i < w_args->steps; i++) {
     // Semaphore wait
-    pthread_mutex_lock(&mtx_lock);
+    res = pthread_mutex_lock(&mtx_lock);
+    may_die(res, "pthread_mutex_lock withdraw wait");
       do {
         h = 0;
         while (bank_accounts_balance[w_args->account_number] + w_args->cash_amount < 0) {
           h++;
-          pthread_cond_wait(&not_enough[w_args->account_number], &mtx_lock);
+          res = pthread_cond_wait(&not_enough[w_args->account_number], &mtx_lock);
+          may_die(res, "pthread_cond_wait withdraw wait not_enough");
         }
         while (sem[w_args->account_number] <= 0) {
           h++;
-          pthread_cond_wait(&operation_cond, &mtx_lock);
+          res = pthread_cond_wait(&operation_cond, &mtx_lock);
+          may_die(res, "pthread_cond_wait withdraw wait sem");
         }
       } while(h != 0);
       sem[w_args->account_number] --;
-    pthread_mutex_unlock(&mtx_lock);
+    res = pthread_mutex_unlock(&mtx_lock);
+    may_die(res, "pthread_mutex_unlock withdraw wait");
     
     // Critic section
 
@@ -144,14 +156,18 @@ void* run_withdraw(void* args) {
     assert(prev_balance + w_args->cash_amount == bank_accounts_balance[w_args->account_number]);
 
     // Semaphore post
-    pthread_mutex_lock(&mtx_lock);
+    res = pthread_mutex_lock(&mtx_lock);
+    may_die(res, "pthread_mutex_lock withdraw post");
       sem[w_args->account_number] ++;
-      pthread_cond_signal(&operation_cond);
+      res = pthread_cond_signal(&operation_cond);
+      may_die(res, "pthread_cond_signal withdraw post sem");
 
       if (w_args->cash_amount > 0) {
-        pthread_cond_signal(&not_enough[w_args->account_number]);
+        res = pthread_cond_signal(&not_enough[w_args->account_number]);
+        may_die(res, "pthread_cond_signal withdraw post not_enough");
       }
-    pthread_mutex_unlock(&mtx_lock);
+    res = pthread_mutex_unlock(&mtx_lock);
+    may_die(res, "pthread_mutex_unlock withdraw post");
     sleep(1);
   }
 
@@ -162,26 +178,30 @@ void* run_transfer(void* args) {
   struct transfer_args* t_args = (struct transfer_args*) args;
   int i;
   int prev_acc_from, prev_acc_to;
-  int h;
+  int h, res;
 
   for(i = 0; i < t_args->steps; i++) {
     // Semaphore wait
-    pthread_mutex_lock(&mtx_lock);
+    res = pthread_mutex_lock(&mtx_lock);
+    may_die(res, "pthread_mutex_lock transfer wait");
       do {
         h = 0;
         while (bank_accounts_balance[t_args->account_from] - t_args->cash_amount < 0) {
           h++;
-          pthread_cond_wait(&not_enough[t_args->account_from], &mtx_lock);
+          res = pthread_cond_wait(&not_enough[t_args->account_from], &mtx_lock);
+          may_die(res, "pthread_cond_wait transfer wait not_enough");
         }
 
         while (sem[t_args->account_from] <= 0 || sem[t_args->account_to] <= 0) {
           h++;
-          pthread_cond_wait(&operation_cond, &mtx_lock);
+          res = pthread_cond_wait(&operation_cond, &mtx_lock);
+          may_die(res, "pthread_cond_wait transfer wait sem");
         }
       } while(h != 0);
       sem[t_args->account_from] --;
       sem[t_args->account_to] --;
-    pthread_mutex_unlock(&mtx_lock);
+    res = pthread_mutex_unlock(&mtx_lock);
+    may_die(res, "pthread_mutex_unlock transfer wait");
 
     // Critic section
     prev_acc_from = bank_accounts_balance[t_args->account_from];
@@ -198,17 +218,28 @@ void* run_transfer(void* args) {
     assert(prev_acc_to + t_args->cash_amount == bank_accounts_balance[t_args->account_to]);
 
     // Semaphore post
-    pthread_mutex_lock(&mtx_lock);
+    res = pthread_mutex_lock(&mtx_lock);
+    may_die(res, "pthread_mutex_lock transfer post");
       sem[t_args->account_from] ++;
       sem[t_args->account_to] ++;
 
-      pthread_cond_signal(&operation_cond);
-      pthread_cond_signal(&not_enough[t_args->account_to]);
-    pthread_mutex_unlock(&mtx_lock);
+      res = pthread_cond_signal(&operation_cond);
+      may_die(res, "pthread_cond_signal transfer post sem");
+      res = pthread_cond_signal(&not_enough[t_args->account_to]);
+      may_die(res, "pthread_cond_signal transfer post not_enough");
+    res = pthread_mutex_unlock(&mtx_lock);
+    may_die(res, "pthread_mutex_unlock transfer post");
 
     sleep(1);
   }
 
   free(t_args);
   return NULL;
+}
+
+void may_die(int res, char* cause) {
+  if (res < 0) {
+    perror(cause);
+    exit(-1);
+  }
 }
